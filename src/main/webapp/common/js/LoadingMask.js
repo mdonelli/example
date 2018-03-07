9788091b75c2cd1414d7aadc9d902eaa4fa6e777
Donevil <mario.donelli@google.com>
2018-03-07 18:58:21 +0100

    mdon loading mask

---
+++ src/main/webapp/common/js/LoadingMask.js
@@ -0,0 +1,33 @@
+LoadingMask = function(targetEl) {
+
+    this.targetEl = targetEl;
+
+    this.init();
+    
+}
+
+LoadingMask.prototype = {
+
+    init: function() {
+
+        this.maskEl = $("<div></div>").attr("class", "loadingMask").hide();
+        $("body").append(this.maskEl);
+    },
+
+    show: function() {
+
+        $(this.maskEl).width($(this.targetEl).outerWidth());
+        $(this.maskEl).height($(this.targetEl).outerHeight());
+
+        var offset = $(this.targetEl).offset();
+
+        $(this.maskEl).show().offset({
+            top: offset.top,
+            left: offset.left
+        });
+    },
+
+    hide: function() {
+        $(this.maskEl).hide();
+    }
+}
